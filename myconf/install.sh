dfac2700c44cf3194d539a81cece4c79d1df3b10
Leander Jedamus <ljedamus@web.de>
2020-12-14 14:14:57 +0100

    added install.sh

---
+++ myconf/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+# erzeugt Montag, 14. Dezember 2020 14:06 (C) 2020 von Leander Jedamus
+# modifiziert Montag, 14. Dezember 2020 14:06 von Leander Jedamus
+
+cp -vp myconf.pl $HOME/bin
+
+# vim:ai sw=2
+
