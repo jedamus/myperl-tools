43b1faa33de403d192f3a47411a76df3aa5331ad
Leander Jedamus <ljedamus@web.de>
2020-12-14 14:15:13 +0100

    added install.sh

---
+++ install.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env sh
+
+# erzeugt Montag, 14. Dezember 2020 14:07 (C) 2020 von Leander Jedamus
+# modifiziert Montag, 14. Dezember 2020 14:07 von Leander Jedamus
+
+cd translate
+./install.sh
+cd ..
+
+cd a2ps
+./install.sh
+cd ..
+
+cd mycopy
+./install.sh
+cd ..
+
+cd myconf
+./install.sh
+cd ..
+
+# vim:ai sw=2
+
