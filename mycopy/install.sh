c6176494b0499d48499eff2824b8741bb11d8844
Leander Jedamus <ljedamus@web.de>
2020-12-14 14:14:50 +0100

    added install.sh

---
+++ mycopy/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+# erzeugt Montag, 14. Dezember 2020 14:02 (C) 2020 von Leander Jedamus
+# modifiziert Montag, 14. Dezember 2020 14:03 von Leander Jedamus
+
+cp -Rvp mycopy.pl mycopy.conf locale $HOME/bin
+
+# vim:ai sw=2
+
