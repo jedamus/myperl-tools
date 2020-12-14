4a7e0eb5c0a6595b78dc2998f83321074545d468
Leander Jedamus <ljedamus@web.de>
2020-12-14 14:19:18 +0100

    added verbose for ln

--- a2ps/install.sh
+++ a2ps/install.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env sh
 
 # erzeugt Montag, 14. Dezember 2020 14:01 (C) 2020 von Leander Jedamus
-# modifiziert Montag, 14. Dezember 2020 14:12 von Leander Jedamus
+# modifiziert Montag, 14. Dezember 2020 14:18 von Leander Jedamus
 
 d=$HOME/bin
 cp -Rvp a2ps.pl locale $d
 cd $d
-ln -sf a2ps.pl a2ps
+ln -svf a2ps.pl a2ps
 
 # vim:ai sw=2
 
