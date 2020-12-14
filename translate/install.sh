4888c85ca1b9778bf17fad31750579bb69d88964
Leander Jedamus <ljedamus@web.de>
2020-12-14 14:14:32 +0100

    added install.sh

---
+++ translate/install.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env sh
+
+# erzeugt Montag, 14. Dezember 2020 13:58 (C) 2020 von Leander Jedamus
+# modifiziert Montag, 14. Dezember 2020 14:01 von Leander Jedamus
+
+cp -vp translate.pl $HOME/bin
+
+# vim:ai sw=2
+
